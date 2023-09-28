5c37798fa297e9e663962a09f3c144fc71fd6f34
luisrangel718 <115975452+luisrangel718@users.noreply.github.com>
2023-09-28 15:23:59 -0500

    Update fantasy_draft.py
    convert to sleeper apis only

--- fantasy_draft.py
+++ fantasy_draft.py
@@ -1,15 +1,17 @@
 import streamlit as st
 import requests
 
-def fetch_team_data(api_key, endpoint):
-    headers = {'Authorization': f'Bearer {api_key}'}
-    response = requests.get(endpoint, headers=headers)
+# Function to fetch your team data
+def fetch_team_data(user_id):
+    url = f'https://api.sleeper.app/v1/user/{user_id}'
+    response = requests.get(url)
     if response.status_code == 200:
         return response.json()
     else:
         st.error(f'Failed to retrieve data: {response.text}')
         return None
 
+# Function to suggest trades (placeholder)
 def suggest_trades(team_data):
     # Implement your trade suggestion logic here
     # For simplicity, returning an empty list
@@ -18,21 +20,19 @@ def suggest_trades(team_data):
 def main():
     st.title("Fantasy Football Trade Suggester")
 
-    api_key = st.text_input("Enter your API key:", type="password")
-    if api_key:
-        st.write("API Key received. Now enter your team endpoint.")
-        team_endpoint = st.text_input("Enter your team endpoint:")
-        if team_endpoint:
-            team_data = fetch_team_data(api_key, team_endpoint)
-            if team_data:
-                st.write("Team data imported successfully!")
-                suggested_trades = suggest_trades(team_data)
-                if suggested_trades:
-                    st.write("Suggested Trades:")
-                    for trade in suggested_trades:
-                        st.write(trade)
-                else:
-                    st.write("No trade suggestions at the moment.")
+    user_id = st.text_input("Enter your Sleeper User ID:")
+    if user_id:
+        team_data = fetch_team_data(user_id)
+        if team_data:
+            st.write("Team data imported successfully!")
+            suggested_trades = suggest_trades(team_data)
+            if suggested_trades:
+                st.write("Suggested Trades:")
+                for trade in suggested_trades:
+                    st.write(trade)
+            else:
+                st.write("No trade suggestions at the moment.")
 
 if __name__ == "__main__":
     main()
+
