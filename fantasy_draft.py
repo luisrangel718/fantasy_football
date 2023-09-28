eee4bef528e76c4416558079509268a0e2751416
luisrangel718 <115975452+luisrangel718@users.noreply.github.com>
2023-09-28 15:32:27 -0500

    Update fantasy_draft.py

--- fantasy_draft.py
+++ fantasy_draft.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import requests
 
-# Function to fetch user data
-def fetch_user_data(identifier):
-    url = f'https://api.sleeper.app/v1/user/{identifier}'
+# Function to fetch rosters data
+def fetch_rosters_data(league_id):
+    url = f'https://api.sleeper.app/v1/league/{league_id}/rosters'
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()
@@ -11,29 +11,22 @@ def fetch_user_data(identifier):
         st.error(f'Failed to retrieve data: {response.text}')
         return None
 
-# Function to suggest trades (placeholder)
-def suggest_trades(user_data):
-    # Implement your trade suggestion logic here
-    # For simplicity, returning an empty list
-    return []
-
 def main():
     st.title("Fantasy Football Trade Suggester")
 
-    identifier = st.text_input("Enter your Sleeper Username or User ID:")
-    if identifier:
-        user_data = fetch_user_data(identifier)
-        if user_data:
-            st.write("User data imported successfully!")
-            st.write(user_data)  # Display the user data
-            suggested_trades = suggest_trades(user_data)
-            if suggested_trades:
-                st.write("Suggested Trades:")
-                for trade in suggested_trades:
-                    st.write(trade)
-            else:
-                st.write("No trade suggestions at the moment.")
+    league_id = st.text_input("Enter your Sleeper League ID:")
+    if league_id:
+        rosters_data = fetch_rosters_data(league_id)
+        if rosters_data:
+            st.write("Rosters data imported successfully!")
+            user_id = st.text_input("Enter your Sleeper User ID to view your team:")
+            if user_id:
+                user_roster = next((roster for roster in rosters_data if roster['owner_id'] == user_id), None)
+                if user_roster:
+                    st.write(f"Team Name: {user_roster['metadata']['team_name']}")
+                    st.write(f"Players: {user_roster['players']}")
+                else:
+                    st.write("No roster found for the provided User ID.")
 
 if __name__ == "__main__":
     main()
-
