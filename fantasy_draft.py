24dba3933b1316d16ccd8e9e6cb87ef70257f54e
luisrangel718 <115975452+luisrangel718@users.noreply.github.com>
2023-09-28 15:26:20 -0500

    Update fantasy_draft.py

--- fantasy_draft.py
+++ fantasy_draft.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import requests
 
-# Function to fetch your team data
-def fetch_team_data(user_id):
-    url = f'https://api.sleeper.app/v1/user/{user_id}'
+# Function to fetch user data
+def fetch_user_data(identifier):
+    url = f'https://api.sleeper.app/v1/user/{identifier}'
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()
@@ -12,7 +12,7 @@ def fetch_team_data(user_id):
         return None
 
 # Function to suggest trades (placeholder)
-def suggest_trades(team_data):
+def suggest_trades(user_data):
     # Implement your trade suggestion logic here
     # For simplicity, returning an empty list
     return []
@@ -20,12 +20,13 @@ def suggest_trades(team_data):
 def main():
     st.title("Fantasy Football Trade Suggester")
 
-    user_id = st.text_input("Enter your Sleeper User ID:")
-    if user_id:
-        team_data = fetch_team_data(user_id)
-        if team_data:
-            st.write("Team data imported successfully!")
-            suggested_trades = suggest_trades(team_data)
+    identifier = st.text_input("Enter your Sleeper Username or User ID:")
+    if identifier:
+        user_data = fetch_user_data(identifier)
+        if user_data:
+            st.write("User data imported successfully!")
+            st.write(user_data)  # Display the user data
+            suggested_trades = suggest_trades(user_data)
             if suggested_trades:
                 st.write("Suggested Trades:")
                 for trade in suggested_trades:
